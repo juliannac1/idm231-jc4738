3473bfe75bc8295f9f6cf0f0387fb3480184cedf
juliannac1 <jc4738@drexel.edu>
2025-03-20 04:36:22 -0400

    usable shelf click

--- main.js
+++ main.js
@@ -27,32 +27,33 @@ if (form) {
 
 
 function findBday(month, day) {
-  if ((month === 12 && day >= 22) || (month === 1 && day <= 19)) {
-    return 'Capricorn';
-  } else if ((month === 11 && day >= 22) || (month === 12 && day <= 21)) {
-    return 'Sagittarius';
-  } else if ((month === 10 && day >= 24) || (month === 11 && day <= 21)) {
-    return 'Scorpio';
-  } else if ((month === 9 && day >= 23) || (month === 10 && day <= 23)) {
-    return 'Libra';
-  } else if ((month === 8 && day >= 23) || (month === 9 && day <= 22)) {
-    return 'Virgo';
-  } else if ((month === 7 && day >= 23) || (month === 8 && day <= 22)) {
-    return 'Leo';
-  } else if ((month === 6 && day >= 22) || (month === 7 && day <= 22)) {
-    return 'Cancer';
-  } else if ((month === 5 && day >= 21) || (month === 6 && day <= 21)) {
-    return 'Gemini';
-  } else if ((month === 4 && day >= 20) || (month === 5 && day <= 20)) {
-    return 'Taurus';
-  } else if ((month === 3 && day >= 21) || (month === 4 && day <= 19)) {
-    return 'Aries';
-  } else if ((month === 2 && day >= 19) || (month === 3 && day <= 20)) {
-    return 'Pisces';
-  } else if ((month === 1 && day >= 20) || (month === 2 && day <= 18)) {
-    return 'Aquarius';
-  } else return null;
-}
+  if ((month == 3 && day >= 21) || (month == 4 && day <= 20)) {
+      intro = "margo";
+  } else if ((month == 4 && day >= 21) || (month == 5 && day <= 20)) {
+      icon = "charlee";
+  } else if ((month == 5 && day >= 21) || (month == 6 && day <= 20)) {
+      icon = "neon";
+  } else if ((month == 6 && day >= 21) || (month == 7 && day <= 22)) {
+      icon = "sparkle";
+  } else if ((month == 7 && day >= 23) || (month == 8 && day <= 22)) {
+      icon = "taglo";
+  } else if ((month == 8 && day >= 23) || (month == 9 && day <= 22)) {
+      icon = "blup";
+  } else if ((month == 9 && day >= 23) || (month == 10 && day <= 22)) {
+      icon = "cider";
+  } else if ((month == 10 && day >= 23) || (month == 11 && day <= 22)) {
+      icon = "pumpkin";
+  } else if ((month == 11 && day >= 23) || (month == 12 && day <= 21)) {
+      icon = "ranch";
+  } else if ((month == 12 && day >= 22) || (month == 1 && day <= 19)) {
+      icon = "boom";
+  } else if ((month == 1 && day >= 20) || (month == 2 && day <= 19)) {
+      icon = "chunk";
+  } else if ((month == 2 && day >= 20) || (month == 3 && day <= 20)) {
+      dish = "mallow";
+  }
+  showIconDetail(icon)
+};
 
 
 function showIconDetail(icon) {
@@ -112,7 +113,7 @@ if (helpButton) {
 }
 
 
-const icons= [
+const iconData= [
     {
         name: 'Boom',
         intro: "Capricorn",
