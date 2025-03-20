4a59f2b5034b5a0718817df1d09b937c912971d3
juliannac1 <jc4738@drexel.edu>
2025-03-20 04:12:42 -0400

    help button

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
+let icon = "";
+let month = "";
+let day = "";
+
+const iconImage = document.getElementById("icon-img");
+const iconName = document.getElementById("icon-name");
+const iconIntro = document.getElementById("icon-intro");
+const iconDescrip = document.getElementById("description");
 
 function enterBday(event) {
     event.preventDefault();
@@ -9,54 +17,66 @@ function enterBday(event) {
     findBday(month, day)
 };
 
-const form = document.querySelector('#bday-generator');
+const form = document.querySelector('#birthday-form');
 if (form) {
     form.addEventListener('submit', enterBday, false);
 } else {
     console.log('no form found')
 }
 
-function getZodiac(month, day) {
-    if ((month === 12 && day >= 22) || (month === 1 && day <= 19)) {
-      return 'Capricorn';
-    } else if ((month === 11 && day >= 22) || (month === 12 && day <= 21)) {
-      return 'Sagittarius';
-    } else if ((month === 10 && day >= 24) || (month === 11 && day <= 21)) {
-      return 'Scorpio';
-    } else if ((month === 9 && day >= 23) || (month === 10 && day <= 23)) {
-      return 'Libra';
-    } else if ((month === 8 && day >= 23) || (month === 9 && day <= 22)) {
-      return 'Virgo';
-    } else if ((month === 7 && day >= 23) || (month === 8 && day <= 22)) {
-      return 'Leo';
-    } else if ((month === 6 && day >= 22) || (month === 7 && day <= 22)) {
-      return 'Cancer';
-    } else if ((month === 5 && day >= 21) || (month === 6 && day <= 21)) {
-      return 'Gemini';
-    } else if ((month === 4 && day >= 20) || (month === 5 && day <= 20)) {
-      return 'Taurus';
-    } else if ((month === 3 && day >= 21) || (month === 4 && day <= 19)) {
-      return 'Aries';
-    } else if ((month === 2 && day >= 19) || (month === 3 && day <= 20)) {
-      return 'Pisces';
-    } else if ((month === 1 && day >= 20) || (month === 2 && day <= 18)) {
-      return 'Aquarius';
-    } else return null;
-  }
+
+
+function findBday(month, day) {
+  if ((month === 12 && day >= 22) || (month === 1 && day <= 19)) {
+    return 'Capricorn';
+  } else if ((month === 11 && day >= 22) || (month === 12 && day <= 21)) {
+    return 'Sagittarius';
+  } else if ((month === 10 && day >= 24) || (month === 11 && day <= 21)) {
+    return 'Scorpio';
+  } else if ((month === 9 && day >= 23) || (month === 10 && day <= 23)) {
+    return 'Libra';
+  } else if ((month === 8 && day >= 23) || (month === 9 && day <= 22)) {
+    return 'Virgo';
+  } else if ((month === 7 && day >= 23) || (month === 8 && day <= 22)) {
+    return 'Leo';
+  } else if ((month === 6 && day >= 22) || (month === 7 && day <= 22)) {
+    return 'Cancer';
+  } else if ((month === 5 && day >= 21) || (month === 6 && day <= 21)) {
+    return 'Gemini';
+  } else if ((month === 4 && day >= 20) || (month === 5 && day <= 20)) {
+    return 'Taurus';
+  } else if ((month === 3 && day >= 21) || (month === 4 && day <= 19)) {
+    return 'Aries';
+  } else if ((month === 2 && day >= 19) || (month === 3 && day <= 20)) {
+    return 'Pisces';
+  } else if ((month === 1 && day >= 20) || (month === 2 && day <= 18)) {
+    return 'Aquarius';
+  } else return null;
+}
+
 
 function showIconDetail(icon) {
-    const icon = iconData.filter(
-        item => item.name === icon
+    const mainIcon = iconData.filter(
+        bud => bud.name === icon
     )[0]
-    console.log(icon)
-    iconImage.src = icon.image;
-    iconName.innerText = icon.name;
-    Intro.innerText = icon.intro;
-    Descrip.innerText = icon.description;
+    console.log(mainIcon)
+    iconImage.src = mainIcon.image;
+    iconName.innerText = mainIcon.name;
+    iconIntro.innerText = mainIcon.intro;
+    iconDescrip.innerText = mainIcon.description;
 
-    playsound(); //add sound
+    playsound(Icon.sound);
 }
-const iconbutton = document.getElementsByClassName('icon-button');
+
+function playsound(sound) {
+    const audioElement = document.getElementById('audioplayer');
+    audioElement.pause();
+    audioElement.currentTime = 0;
+    audioElement.src = sound;
+    audioElement.play();
+}
+
+const iconButton = document.getElementsByClassName('icon-button');
 
 for (const button of iconButton) {
     button.addEventListener('click', () => {
@@ -64,21 +84,40 @@ for (const button of iconButton) {
     }, false);
 }
 
-function getInfo(zodiac) {
-    for (let icon of icons) {
-        if (icon.zodiac == zodiac) {
-            let info = [icon.name, icon.intro, icon.image, icon.sound, album.descrip];
-            return info;
-        }
-    }
+
+function showClickedIcon(button) {
+    const buddy = button.id;
+    const mainIcon = iconData.filter(
+        bud => bud.id === buddy
+    )[0]
+    console.log(mainIcon)
+    iconImage.src = mainIcon.image;
+    iconName.innerText = mainIcon.name;
+    iconIntro.innerText = mainIcon.intro;
+    iconDescrip.innerText = mainIcon.description;
+
+    playsound(mainIcon.sound);
 }
+
+const helpButton = document.getElementById('help-button');
+
+if (helpButton) {
+    helpButton.addEventListener('click', () => {
+        iconImage.src = 'image-asset/icon-maindish.png';//change this part
+        iconName.innerText = 'Instruction';
+        iconName.style.paddingTop = '1.2rem';
+        iconIntro.innerText = 'Welcome to ByteBuds!';
+        iconDescrip.innerText = 'Enter your birthday above to find out which ByteBud represents you! You can also explore the buttons on the side to learn about other Buddies and their descriptions.';
+    }, false);
+}
+
 
 const icons= [
     {
         name: 'Boom',
         intro: "Capricorn",
         description: "You are friendly and outgoing, known as 'the diplomat' in your friend group. You are good at debating and public speaking. You are a wonderful story teller - it's hard to get distracted when listening to you!",
-        image: 'characters/boom.png',
+        image: 'images/characters/boom.capricornus.png',
         sound: 'sounds/boom.wav',
         id: "boom",
     },
@@ -170,34 +209,5 @@ const icons= [
         sound: 'sounds/ranch.wav',
         id: "ranch",
     }];
+  
     
-    
-
-    const helpButton = document.getElementById('help-button');
-    const help_container = document.querySelector('#help-container');
-   
-    if (helpButton) {
-        helpButton.addEventListener('click', () => {
-            iconImage.src = 'image-asset/icon-mainicon.png';
-            iconName.innerText = 'Instruction';
-            icon.style.paddingTop = '1.2rem';
-            Intro.innerText = 'Welcome to the icon Generator!';
-            descrip.innerText = 'Enter your birthday above to discover whichicon is your new friend! You can also click on buttons on the side to read about other icons and corresponding characters.';
-        }, false);
-    }
-
-    const help_close_btn = document.querySelector('.help-close-button')
-
-if (help_close_btn) {}
-  help_close_btn.addEventListener ('click', () => {
-    if (help_container.style.display === 'flex') {
-      help_container.style.display = 'none';
-}})
-
-    const overlay = document.createElement('div');
-    overlay.className = 'overlay';
-
-    if (form) {
-        form.addEventListener('submit', handle_submit);
-      } 
-      
